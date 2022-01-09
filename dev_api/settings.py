a7f0aa314f9ec4959088c407005913e97eb02f46
Divine Ikhuoria <64232397+Divuzki@users.noreply.github.com>
2022-01-09 09:48:30 +0100

    Updated settings.py

--- dev_api/settings.py
+++ dev_api/settings.py
@@ -115,7 +115,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
